e0b36dbcd42868f77fd8d06381a9f4d66b158678
dj04304 <dj043045@gmail.com>
2022-08-08 19:14:27 +0900

    0808 시큐리티 공부 시작

---
+++ src/main/java/com/study/security_junhyeong/SecurityJunhyeongApplication.java
@@ -0,0 +1,13 @@
+package com.study.security_junhyeong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJunhyeongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityJunhyeongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/security_junhyeong/SecurityJunhyeongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.security_junhyeong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityJunhyeongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
