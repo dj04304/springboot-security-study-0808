faefcbb5b5cd99c2571ef81b14b3407f2bdf3213
dj04304 <dj043045@gmail.com>
2022-08-08 21:51:35 +0900

    시큐리티 수업 1일차

---
+++ src/main/java/com/study/security_junhyeong/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.study.security_junhyeong.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.study.security_junhyeong.config.auth.AuthFailureHandler;
+
+@EnableWebSecurity // 기존의 WebSecurityConfigurerAdapter를 비활성화 시키고, 현재 Security 설정을 따르겠다.
+@Configuration // IoC에 등록
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() { //BCryptPasswordEncoder 는 암호화이다.
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+	@Override // super.configure(http); ->  기존 security 방법 //http 는 builder형식임
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable(); // csrf 를 사용하는 것을 권장하지만, 지금은 사용하면 문제가 생긴다. input 마다 다 지급을 해줘야 하기 때문에
+		http.authorizeRequests() // 요청이 들어왔을때의 인증 세팅
+				.antMatchers("/", "/index", "/mypage/**") //우리가 지정한 요청
+				.authenticated() 					// 우리가 지정한 요청은 인증을 거쳐라
+				.anyRequest() 						// 다른 모든 요청들에게는 
+				.permitAll()							// 모든 접근 권한(permitAll)을 부여해라(인증을 거칠 필요가 없다.)
+				.and() //여기까지 한 묶음
+				.formLogin() // 로그인 방법 3가지 중 하나 (form로그인, 베이직 로그인, JWT)
+				.loginPage("/auth/signin") // 로그인 페이지는 해당 get요청을 통해 접근한다.
+				.loginProcessingUrl("/auth/signin") // 로그인 요청(post요청)
+				.failureHandler(new AuthFailureHandler()) //직접 커스텀한 handler 를 넣어준다.
+				.defaultSuccessUrl("/"); //처음 로그인 페이지로 들어가서 로그인을 했을 때, 보내주는 경로, 단 예를들어 mypage로 들어가서 로그인을 할 경우, mypage로 보내준다.
+														//즉 처음 시작한 페이지가 로그인 이외의 페이지라면, 그 곳으로 보내주지만, login 페이지부터 시작할 경우 defaultSuccessUrl에서 정해준 경로로 보내준다.
+	}
+	
+}
---
+++ src/main/java/com/study/security_junhyeong/domain/user/User.java
@@ -0,0 +1,38 @@
+package com.study.security_junhyeong.domain.user;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+	private int user_code;
+	private String user_name;
+	private String user_email;
+	private String user_id;
+	private String user_password;
+	private String user_roles; 				//ROLE_USER, ROLE_ADMIN, ROLE_MANAGER
+	private String user_provider;
+	private String user_profile_img;
+	private String user_address;
+	private String user_phone;
+	private int user_gender;
+	
+	public List<String> getUserRoles() {
+		
+		if(user_roles == null) {
+			return new ArrayList<String>();
+		}
+		return Arrays.asList(user_roles.split(",")); //쉼표단위로 끊어서 배열을 만들어준다. 그 배열을 list로 만들겠다.
+		
+	}
+}
